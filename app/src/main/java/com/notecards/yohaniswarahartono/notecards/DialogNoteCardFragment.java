0904a507c5ea3683940963414862137db2216e48
crdnblu <crdnblu@ymail.com>
2015-12-06 02:43:29 -0600

    Revert "changed"
    This reverts commit 6b6c902e894b2eb4938dfbb250c618b9fbbe058c.

--- app/src/main/java/com/notecards/yohaniswarahartono/notecards/DialogNoteCardFragment.java
+++ app/src/main/java/com/notecards/yohaniswarahartono/notecards/DialogNoteCardFragment.java
@@ -9,32 +9,23 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.Date;
 import java.util.UUID;
 
-/**
- * Created by Alfin Rahardja on 12/4/2015.
- */
+/**********************************************************************/
+/*                   Dialog to create new notecard                    */
+/**********************************************************************/
 public class DialogNoteCardFragment extends DialogFragment {
     // Constant Variables
-    private static final String ARG_SUBJECT = "subject";
-
-    // Layout
-    private EditText topic_edit;
-    private EditText front_edit;
-    private EditText back_edit;
-    private Button   date_button;
-
+    private static final String ARG_SUBJECT = "subject"; // Tag to get the subject
 
     // Member Variables
-    private NoteCard newNoteCard;
-    private Subject mSubject;
-    private UUID     mSubjectId;
-    private NoteSingleton singleton = NoteSingleton.get();
-
+    private EditText name;          // user input for notecard's name
+    private NoteCard newNoteCard;   // New notecard
+    private Subject mSubject;       // Subject of the notecard
+    private UUID     mSubjectId;    // The unique subject ID
+    private NoteSingleton singleton = NoteSingleton.get();  // Singleton
 
     public static DialogNoteCardFragment newInstance(UUID subjectId){
         Bundle args = new Bundle();
@@ -48,19 +39,13 @@ public class DialogNoteCardFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
-
         View v = LayoutInflater.from(getActivity())
-                .inflate(R.layout.add_notecard_dialog, null);
+                .inflate(R.layout.add_subject_dialog, null);
 
         mSubjectId = (UUID) getArguments().getSerializable(ARG_SUBJECT);
         mSubject = singleton.getSubject(mSubjectId);
 
-        topic_edit  = (EditText) v.findViewById(R.id.add_note_card_edit);
-        front_edit  = (EditText) v.findViewById(R.id.add_note_card_edit_front);
-        back_edit   = (EditText) v.findViewById(R.id.add_note_card_edit_back);
-        date_button = (Button)   v.findViewById(R.id.add_note_card_date);
-        date_button.setText(new Date().toString());
-
+        name = (EditText) v.findViewById(R.id.add_subject_edit_name);
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
@@ -70,12 +55,9 @@ public class DialogNoteCardFragment extends DialogFragment {
                             @Override
                             public void onClick(DialogInterface dialog, int which)
                             {
-
+                                String title = name.getText().toString();
                                 newNoteCard = new NoteCard();
-                                newNoteCard.setNoteCardTitle(topic_edit.getText().toString());
-                                newNoteCard.setFrontSide(front_edit.getText().toString());
-                                newNoteCard.setBackSide(back_edit.getText().toString());
-
+                                newNoteCard.setNoteCardTitle(title);
                                 singleton.addNoteCard(newNoteCard, mSubject);
                                 notifyToTarget(Activity.RESULT_OK);
                             }
